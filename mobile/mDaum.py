fb20fcdbbbda0d8ece813f01be0e94ae8b1c4daf
Jaeyoung Lee <jaeyoung@monodiary.net>
2013-04-16 23:37:39 +0900

    add get_article_list to mDaum.py

--- mobile/mDaum.py
+++ mobile/mDaum.py
@@ -8,6 +8,12 @@ from dateutil import parser as DATE
 import requests
 
 
+UserAgent = """
+        Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us)\
+        AppleWebKit/420.1 (KHTML, like Gecko) Version/3.0 Mobile/1A542a Safari/419.3
+        """
+
+
 def main():
     print get_article("http://blog.daum.net/phjsunflower/1100")
 
@@ -17,11 +23,7 @@ def get_article(url, mode=None):
     returnee = {}
 
     if not mode:
-        agent = """
-        Mozilla/5.0 (iPhone; U; CPU iPhone OS 3_0 like Mac OS X; en-us)\
-        AppleWebKit/420.1 (KHTML, like Gecko) Version/3.0 Mobile/1A542a Safari/419.3
-        """
-        structure = requests.get(url, headers={"User-Agent": agent})
+        structure = requests.get(url, headers={"User-Agent": UserAgent})
 
     else:
         structure = mode
@@ -38,7 +40,7 @@ def get_article(url, mode=None):
     article = body.cssselect("div#article")[0]
     returnee["content"] = st.strip_html(html.tostring(article, encoding=charset, method="html")).replace("\t", "")
     returnee["images"] = get_images(article)
-    returnee["post_id"] = url[url.rfind("/")+1:]
+    returnee["post_id"] = url[url.rfind("/") + 1:]
     return returnee
 
 
@@ -51,5 +53,42 @@ def get_images(article):
     return urls
 
 
+def get_article_list(host, lp=None):
+    returnee = []
+    prefix = "http://m.blog.daum.net"
+
+    current_page, flag, tmp = 1, 1, 1
+    next_page = host
+
+    while flag:
+        re = requests.get(next_page, headers={"User-Agent": UserAgent})
+        tree = html.fromstring(re.text)
+        tmp = current_page
+
+        num_pages = tree.cssselect("a.num_box")  # find next page
+        for num_page in num_pages:
+            chk = "currentPage=%d" % (current_page + 1)
+            if chk in num_page.get("href"):
+                next_page = prefix + num_page.get("href")
+                current_page += 1
+                break
+
+        if tmp == current_page:
+            flag = 0
+
+        articles = tree.cssselect("div.list_by_user")[0].cssselect("li")  # get article_list
+        for article in articles:
+            url = prefix + article.cssselect("a")[0].get("href")
+            date = article.cssselect("span.date")[0].text
+            if lp and (DATE.parse(date) - DATE.parse(lp)).days < 0:
+                flag = 0
+                break
+
+            else:
+                returnee.append(url)
+
+    return returnee
+
 if __name__ == "__main__":
     main()
+    # print len(get_article_list("http://blog.daum.net/hamami10", "2012.10.31 08:56"))
