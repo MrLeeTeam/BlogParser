33d0b1102197c10e6532f11078816d24c7398d25
redd0g <eceipeno@gamil.com>
2013-05-01 14:03:26 +0900

    move db code to database module

---
+++ database.py
@@ -0,0 +1,78 @@
+import psycopg2
+import datetime
+
+
+con = None
+crawler_id = None
+
+def init(cr_id):
+    global con
+    global crawler_id
+    con = psycopg2.connect(host="61.43.139.115", database="mrlee", user="mrlee", password="altmxjfl")
+    crawler_id = cr_id
+
+def save_article(b_id, data):
+    global con
+    try:
+
+        cursor = con.cursor()
+        cursor.execute("INSERT INTO blog_data(b_id, post_id, author, title, crawl_date, post_date, contents) values(%s, %s, %s, %s, %s, %s, %s)", [b_id, data["post_id"], data["name"], data["title"], datetime.datetime.now(), data["date"], data["content"]])
+        con.commit()
+
+        image_list = [[b_id, data["post_id"], url, datetime.datetime.now()] for url in data["images"]]
+        for image in image_list:
+            cursor.execute("INSERT INTO blog_image(b_id, post_id, image_url, crawl_date) VALUES(%s, %s, %s, %s)", image)
+            con.commit()
+
+    except Exception, e:
+        print "## save error : %s " % e.message
+        con.rollback()
+
+
+
+
+def flag(b_id, sw):
+    global con
+
+    try:
+        cursor = con.cursor()
+
+        cursor.execute("UPDATE blog_meta set crawler_id = 0, last_crawl = %s where b_id = %s", [datetime.datetime.now(), b_id])
+        con.commit()
+
+    except:
+        print "set flag error"
+        if con:
+            con.rollback()
+
+
+
+def get_meta():  # Set Flag, Get Host, Get Realm, Get Date
+    global con
+    global crawler_id
+    b_id = None
+    url = None
+    realm = None
+    last_crawl = None
+    last_post = None
+    succ_flag = False
+
+
+    try:
+        cursor = con.cursor()
+        cursor.execute("select b_id, url, realm, last_crawl, last_post from blog_meta where last_crawl is null and (crawler_id is null or crawler_id = 0) and realm in ( 'Daum')   limit 1")
+
+        record = cursor.fetchone()
+        if record:
+            b_id, url, realm, last_crawl, last_post = record
+            succ_flag = True
+
+            cursor.execute("UPDATE blog_meta set crawler_id = %d where b_id = %d " % (crawler_id, b_id))
+            con.commit()
+
+    except:
+        print "get meta error"
+        if con:
+            con.rollback()
+
+    return b_id, url, realm, last_crawl, last_post, succ_flag
--- parser.py
+++ parser.py
@@ -1,10 +1,10 @@
 __author__ = 'jaeyoung'
 
-
 from mobile import mTistory, mDaum, mEgloos, mNaver
-
+import sys
 import requests
-import psycopg2 as psycopg2
+import database
+import datetime
 
 # -*- Constant -*-
 UserAgent = """
@@ -12,149 +12,93 @@ UserAgent = """
             AppleWebKit/420.1 (KHTML, like Gecko) Version/3.0 Mobile/1A542a Safari/419.3
             """
 
-CRAWLER_ID = 1
+crawler_id = 0
+
+
+def init():
+    global crawler_id
+    if len(sys.argv) < 2:
+        print "Usage: %s [crawler id]" % sys.argv[0]
+        sys.exit(1)
+
+    crawler_id = int(sys.argv[1])
+    database.init(crawler_id)
+
 
 def main():
-    b_id, host, realm, last_crawl, last_post, succeed = get_meta()
-    if succeed == False:
-        print "SUCCEED value false"
-        return
-    #flag(h_id, 1)  # Set flag
-    article_list = get_article_list(host, realm, last_post)
+    while True:
+        b_id, host, realm, last_crawl, last_post, succeed = database.get_meta()
 
-    for article in article_list:  # parse article and save to database
-        data = get_article(article, realm)
-        save(data, b_id)
+        if not succeed:
+            print "SUCCEED value false"
+            return
 
-    flag(b_id, 0)  # Unset flag
+        print "["+datetime.datetime.now(), "] : ", host,
 
+        article_list = get_article_list(host, realm, last_post)
+        print " [",len(article_list),"]"
+        for article in article_list:  # parse article and save to database
+            data = get_article(article, realm)
+            if len(data) == 0 : continue
+            database.save_article(b_id, data)
 
-def get_meta():  # Set Flag, Get Host, Get Realm, Get Date
-
-    b_id = None
-    url = None
-    realm = None
-    last_crawl = None
-    last_post = None
-    succ_flag = False
-
-    conn = None
-    try:
-        conn = psycopg2.connect(host="61.43.139.115", database="mrlee", user="mrlee", password="altmxjfl")
-        cursor = conn.cursor()
-        cursor.execute("select b_id, url, realm, last_crawl, last_post from blog_meta where  (crawler_id is null or crawler_id = 0) and realm not in ('Daum', 'Naver') order by last_crawl  limit 1")
-
-        record = cursor.fetchone()
-        if record:
-            b_id, url, realm, last_crawl, last_post = record
-            succ_flag = True
-
-            cursor.execute("UPDATE blog_meta set crawler_id = %d where b_id = %d " % (CRAWLER_ID, b_id))
-            conn.commit()
+        database.flag(b_id, 0)  # Unset flag
 
 
-    except:
-        print "get meta error"
-        if conn:
-            conn.rollback()
-
-    finally:
-        if conn:
-            conn.close()
-
-    return b_id, url, realm, last_crawl, last_post, succ_flag
-
-
-def flag(b_id, sw):
-    # ids = ""
-    # for ido in h_id:
-    #     ids += "%s" % ido if len(ids) == 0 else ",%s" % ido
-    #
-    # if sw == 0:
-    #     # Unset Flag
-    #     pass
-    #
-    # elif sw == 1:
-    #     # Set Flag
-    #     pass
-
-    conn = None
-    try:
-        conn = psycopg2.connect(host="61.43.139.115", database="mrlee", user="mrlee", password="altmxjfl")
-        cursor = conn.cursor()
-
-        cursor.execute("UPDATE blog_meta set crawler_id = 0 where b_id = %d " % (b_id))
-        conn.commit()
-
-    except:
-        print "set flag error"
-        if conn:
-            conn.rollback()
-
-    finally:
-        if conn:
-            conn.close()
 
 
 def get_article_list(host, realm=None, lp=None):
     if host.find("http://") == -1:
         host = "http://" + host
-    re = requests.get(host, headers={"User-agent": UserAgent})
+    re = requests.get(host, headers={"User-agent": UserAgent}, timeout=5.0)
+
     article_list = []
-
-    if realm == "Tistory" or "tistory.com" in re.text:
-        article_list = mTistory.get_article_list(host, lp)
-    #
-    # elif realm == "Daum" or "blog.daum.net" in host:
-    #     pass
-    #
-    # elif realm == "Naver" or "naver.com" in re.text:
-    #     pass
-
-    elif realm == "Egloos" or "egloos.com" in re.text:
-        article_list = mEgloos.get_article_list(host, lp)
-
-    return article_list
-
-
-def save(data, b_id):  # Save data to db
-    conn = None
+    if re.status_code == 404:
+        return article_list
     try:
-        conn = psycopg2.connect(host="61.43.139.115", database="mrlee", user="mrlee", password="altmxjfl")
-        cursor = conn.cursor()
-        cursor.execute("INSERT INTO blog_data(b_id, post_id, author, title, postdate, contents) values(%s, %s, %s, %s, %s, %s)", [b_id, data["post_id]"], data["name"], data["title"], data["date"], data["content"]])
-        conn.commit()
+        if realm == "Tistory" or "tistory.com" in re.text:
+            article_list = mTistory.get_article_list(host, lp)
+        #
+        elif realm == "Daum" or "blog.daum.net" in host:
+            article_list = mDaum.get_article_list(host, lp)
 
-    except:
-        print "## save error"
+        elif realm == "Naver" or "naver.com" in re.text:
+            article_list = mNaver.get_article_list(host, lp)
 
-    finally:
-        if conn:
-            conn.close()
+        elif realm == "Egloos" or "egloos.com" in re.text:
+            article_list = mEgloos.get_article_list(host, lp)
+    except IndexError:
+        pass
+    return article_list
 
 
 def get_article(url, realm=None):
     if url.find("http://") == -1:
         url = "http://" + url
 
-    re = requests.get(url, headers={"User-agent": UserAgent})
+    re = requests.get(url, headers={"User-agent": UserAgent}, timeout=5.0)
     data = {}
 
-    if realm == "Tistory" or "tistory.com" in re.text:
-        data = mTistory.get_article(url, re)
+    if re.status_code == 404:
+        return data
+    try:
+        if realm == "Tistory" or "tistory.com" in re.text:
+            data = mTistory.get_article(url, re)
 
-    elif realm == "Daum" or "blog.daum.net" in url:
-        data = mDaum.get_article(url, re)
+        elif realm == "Daum" or "blog.daum.net" in url:
+            data = mDaum.get_article(url, re)
 
-    elif realm == "Naver" or "naver.com" in re.text:
-        data = mNaver.get_article(url, re)
+        elif realm == "Naver" or "naver.com" in re.text:
+            data = mNaver.get_article(url, re)
 
-    elif realm == "Egloos" or "egloos.com" in re.text:
-        data = mEgloos.get_article(url, re)
+        elif realm == "Egloos" or "egloos.com" in re.text:
+            data = mEgloos.get_article(url, re)
+    except IndexError:
+        pass
 
     return data
 
 
 if __name__ == "__main__":
+    init()
     main()
-    # flag(["15", "19", "20", "26", "27", "29"], 0)
